a1087f652552fd95025ca1010d9a5c711ceb4b43
asolanky <alwinsolanky@gmail.com>
2018-04-14 10:29:24 +0100

    Added operators and strings basics

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ A place with Python learning exercises stored.
 
 https://cocalc.com/projects?session=default
 
-login
+login to Anaconda a great python IDE Jupyter and syper.
+
+Order of operation matters
--- simple.py
+++ simple.py
@@ -16,3 +16,18 @@ print("5 / 2 = ", 5/2)
 print("5 % 2 = ", 5%2)
 print("5 ** 2 = ", 5**2)
 print("5 // 2 = ", 5//2)
+print()
+print("1 + 2 - 3 * 2 = ", 1 + 2 - 3 * 2)
+print()
+print("(1 + 2 - 3) * 2 = ", (1 + 2 - 3) * 2)
+
+quote = "\"Always remember you are You!"
+
+multi_line_quote = ''' Just
+like everyone else'''
+
+new_string = quote + multi_line_quote
+
+print(quote)
+print(multi_line_quote)
+print(new_string)
