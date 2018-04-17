66a7927dd286bae9950cf1ed898600c11560b0e2
asolanky <alwinsolanky@gmail.com>
2018-04-17 12:20:04 +0100

    New program list

---
+++ lists.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 12:12:13 2018
+
+@author: alwin
+"""
+
+import random
+import sys
+import os
+
+
+grocery_list = ['Juice', 'Leeks', 'Potatoes', 'Bananas']
+
+print('First Item:', grocery_list[0])
