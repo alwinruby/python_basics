6aa7098e72c409137484c10837dfcaaefe1f9ad2
asolanky <alwinsolanky@gmail.com>
2018-04-18 09:32:30 +0100

    Added more for lists

--- lists.py
+++ lists.py
@@ -14,3 +14,32 @@ import os
 grocery_list = ['Juice', 'Leeks', 'Potatoes', 'Bananas']
 
 print('First Item:', grocery_list[0])
+print()
+print(grocery_list[1:3])
+other_events = ['Wash Car', 'Pick up kids', 'Car check']
+
+to_do_list = [other_events, grocery_list]
+print()
+print(to_do_list)
+print()
+print((to_do_list[1][1]))
+grocery_list.append('Onions')
+print()
+print(to_do_list)
+print()
+grocery_list.insert(1, 'Pickle')
+print()
+print(to_do_list)
+print()
+grocery_list.remove('Pickle')
+print()
+print(to_do_list)
+print()
+grocery_list.sort()
+print()
+print(to_do_list)
+print()
+grocery_list.reverse()
+print()
+print(to_do_list)
+print()
