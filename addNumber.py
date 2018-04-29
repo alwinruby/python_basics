0945795af1b9397b26f914be2676e5e1061593a6
asolanky <alwinsolanky@gmail.com>
2018-04-29 13:17:54 +0100

    addNumber

---
+++ addNumber.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 13:05:24 2018
+
+@author: alwin
+"""
+
+import random
+import sys
+import os
+
+def addNumber(fNum, lNum):
+    sumNum = fNum + lNum
+    return sumNum
+
+
+print(addNumber(1, 4))
+
+#string = addNumber(2, 4)
+
+#print(sumNum) - sum Num is OUT of scope
