bf8aaf08ba700470b842f959518072ab87639abe
asolanky <alwinsolanky@gmail.com>
2018-04-29 13:40:13 +0100

    strings

---
+++ strings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 13:30:55 2018
+
+@author: alwin
+"""
+
+import random
+import sys
+import os
+
+long_string = "I'll catch you if you fall - The Floor"
+print(long_string)
+print('\n')
+print(long_string[0:4])
+print('\n')
+print(long_string[-5:])
+print('\n')
+print(long_string[:-5])
+print('\n')
+print(long_string[:4] + " be there")
+print('\n')
+print("%c is my %s letter  and my number %d number is %.5f" % 
+      ('X', 'favourite', 1, .14))
