a1f4b72de647148c4012a0b3ea1fe1b720eeb8a9
asolanky <alwinsolanky@gmail.com>
2018-04-29 13:48:28 +0100

    strings2

---
+++ strings2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 13:30:55 2018
+
+@author: alwin
+"""
+
+import random
+import sys
+import os
+
+long_string = "I'll catch you if you fall - The Floor"
+print(long_string)
+print('\n')
+print(long_string.capitalize())
+print('\n')
+print(long_string.find("Floor"))
+print('\n')
+print(long_string.isalpha())
+print('\n')
+print(long_string.isalnum())
+print('\n')
+print(len(long_string))
+print('\n')
+print(long_string.replace("Floor", "Ground"))
+print('\n')
+print(long_string.strip())
+print('\n')
+quote_list = long_string.split(" ")
+print(quote_list)
