2234b4bd5c4d1379db2c32c096af8522980c3edb
asolanky <alwinsolanky@gmail.com>
2018-04-29 13:30:24 +0100

    consoleInput

---
+++ consoleInput.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 13:23:43 2018
+
+@author: alwin
+"""
+
+import random
+import sys
+import os
+
+print('What is your name?')
+
+name = sys.stdin.readline()
+
+print('Hello', name)
