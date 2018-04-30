d16d006d4488e59d65f5c5912b8fe9dda1f843e2
asolanky <alwinsolanky@gmail.com>
2018-04-30 14:15:48 +0100

    Inheritance and polymorphism

--- objects.py
+++ objects.py
@@ -50,10 +50,54 @@ class Animal:
         print("Animal")
 
     def toString(self):
-        return "{} is {} cm tall and  {} kg and says {}".format(self.__name,
+        return "{} is {} cm tall and {} kg and says {}".format(self.__name,
                 self.__height, self.__weight, self.__sound)
 
 
 cat = Animal('Whiskers', 33, 10, 'Purr')
 
 print(cat.toString())
+
+
+class Dog(Animal):
+    __owner = None
+
+    def __init__(self, name, height, weight, sound, owner):
+        self.__owner = owner
+        self.__animal_type = None
+
+        super(Dog, self).__init__(name, height, weight, sound)
+
+    def set_owner(self, owner):
+        self.__owner = owner
+
+    def get_owner(self):
+        return self.__owner
+
+    def get_type(self):
+        print ("Dog")
+
+    def toString(self):
+        return "{} is {} cm tall and {} kilograms and says {}. His owner is {}".format(self.get_name(), self.get_height(), self.get_weight(), self.get_sound(), self.__owner)
+
+    def multiple_sounds(self, how_many=None):
+        if how_many is None:
+            print(self.get_sound)
+        else:
+            print(self.get_sound() * how_many)
+
+spot = Dog("Spot", 53, 27, "Ruff", "Derek")
+
+print(spot.toString())
+
+
+class AnimalTesting:
+    def get_type(self, animal):
+        animal.get_type()
+
+test_animals = AnimalTesting()
+
+test_animals.get_type(cat)
+test_animals.get_type(spot)
+
+spot.multiple_sounds(4)
