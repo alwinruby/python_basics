f91f7d45cbdcd13b4c2ea387d8d286b34d34f364
asolanky <alwinsolanky@gmail.com>
2018-05-17 18:33:47 +0100

    quick update to add direction function

--- direct_me/direction.py
+++ direct_me/direction.py
@@ -1,8 +1,41 @@
+
+# Create a function to calculate the arrival time.
+# arrival time = current time + duration, you can use timedelta for this.
+# Create a seperate function which takes all the data as the input and writes it to a html file.
+# Get the right address to enter by using Google's address auto complete
+
+
 # Use Python's inbuilt input() function to get the required data.
 
 import requests
 from datetime import datetime
 from local import API_KEY
+
+
+# Create a seperate function to make the API call and return the data.
+
+def get_direction(starting_point, destination_point, mode_of_travel):
+
+    starting_point = "+".join(starting_point.split(" "))
+
+    destination_point = "+".join(destination_point.split(" "))
+
+    request_url = 'https://maps.googleapis.com/maps/api/directions/json?origin={0}&destination={1}&mode={2}&key={3}'.format(origin,destination,travel_mode,API_KEY)
+
+    response = requests.get(request_url)
+
+    data = response.json()
+
+    if data['status'] == 'OK':
+        print('OK')
+        return data
+    elif data['status'] == 'NOT_FOUND':
+        raise ValueError('The result is not found, enter a valid origin and destination address.')
+    elif data['status'] == 'MAX_ROUTE_LENGTH_EXCEEDED':
+        raise ValueError('The max route length has been exceeded, enter a valid origin and destination address to travel between.')
+    elif data['status'] == 'ZERO_RESULTS':
+        raise Exception('No results are found for your chosen options.')
+
 
 
 if __name__ == "__main__":
@@ -32,13 +65,3 @@ if __name__ == "__main__":
         raise ValueError('The entered, is not vaild.')
 
     print("You are travelling by : " + mode_of_travel)
-
-
-# Create a seperate function to make the API call and return the data.
-
-def get_direction()
-
-# Create a function to calculate the arrival time.
-# arrival time = current time + duration, you can use timedelta for this.
-# Create a seperate function which takes all the data as the input and writes it to a html file.
-# Get the right address to enter by using Google's address auto complete
